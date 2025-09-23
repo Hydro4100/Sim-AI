73d60c4a8965288b13203975ed0fd3bf13fbd21a
Hydro4100 <ben.trewick@gmail.com>
2025-09-23 12:00:33 +0100

    Add project files.

---
+++ MainQuest1-ClosestToTen/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MainQuest1_ClosestToTen
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Rectangle _rectangle;
+        private Texture2D _whitePixelTexture;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            int rectangleWidth = 200;
+            int rectangleHeight = 100;
+            int x = (GraphicsDevice.Viewport.Width - rectangleWidth) / 2; //Screen size divided by 2 and offset by half rectangle size
+            int y = (GraphicsDevice.Viewport.Height - rectangleHeight) / 2;
+            _rectangle = new Rectangle(x, y, rectangleWidth, rectangleHeight);
+
+            _whitePixelTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _whitePixelTexture.SetData(new Color[] { Color.White });
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_whitePixelTexture, _rectangle, Color.White);
+
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
