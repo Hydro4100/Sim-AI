e7b5e6eb9079fa27f66212aa7253b277cb9f0baf
Hydro4100 <ben.trewick@gmail.com>
2025-09-30 12:19:12 +0100

    Can draw a cricle, triangle and square

--- MainQuest2_SuperStroop/Game2.cs
+++ MainQuest2_SuperStroop/Game2.cs
@@ -10,8 +10,12 @@ namespace MainQuest2_SuperStroop
         private SpriteBatch _spriteBatch;
 
         private Rectangle _rectangle;
-        private 
-        private Rectangle _rectangle;
+        private Rectangle _circle;
+        private Rectangle _triangle;
+
+        private Texture2D _rectangleTexture;
+        private Texture2D _circleTexture;
+        private Texture2D _triangleTexture;
 
         public Game2()
         {
@@ -32,6 +36,12 @@ namespace MainQuest2_SuperStroop
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            _rectangleTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _rectangleTexture.SetData(new Color[] { Color.White });
+
+            _circleTexture = Content.Load<Texture2D>("circle");
+            _triangleTexture = Content.Load<Texture2D>("triangle");
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,6 +59,13 @@ namespace MainQuest2_SuperStroop
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_rectangleTexture, new Rectangle(100, 100, 30, 30), Color.Red);
+            _spriteBatch.Draw(_circleTexture, new Rectangle(150, 100, 30, 30), Color.Yellow);
+            _spriteBatch.Draw(_triangleTexture, new Rectangle(200, 100, 30, 30), Color.Green);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
