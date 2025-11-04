221c6030181f0c0493dcd90e7e2df6a83a853669
Hydro4100 <ben.trewick@gmail.com>
2025-11-04 21:28:10 +0000

    Created a generic sparse graph data structure

---
+++ MainQuest5_PacManGhost/SparseGraph.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MainQuest5_PacManGhost
+{
+    public class SparseGraph<TNode, TEdge>
+    {
+        private Dictionary<TNode, List<(TEdge edge, TNode node)>> _adjacency;
+
+        public SparseGraph()
+        {
+            _adjacency = new Dictionary<TNode, List<(TEdge edge, TNode node)>>();
+        }
+
+        public void AddNode(TNode node)
+        {
+            if (!_adjacency.ContainsKey(node))
+            {
+                _adjacency[node] = new List<(TEdge edge, TNode node)>();
+            }
+        }
+
+        public void AddEdge(TNode from, TEdge edge, TNode to)
+        {
+            AddNode(from);
+            AddNode(to);
+
+            _adjacency[from].Add((edge, to));
+        }
+
+        public IEnumerable<(TEdge edge, TNode node)> GetEdges(TNode node)
+        {
+            if (_adjacency.ContainsKey(node))
+            {
+                return _adjacency[node];
+            }
+
+            return Enumerable.Empty<(TEdge edge, TNode node)>();
+        }
+
+        public ICollection<TNode> GetAllNodes()
+        {
+            return _adjacency.Keys;
+        }
+    }
+}
